0cd39032348dc3c28de96e357db58983fea69b8a
Boato Andrea <163646394+aandrea-boatoo@users.noreply.github.com>
2024-11-03 15:30:16 +0100

    prova es 10

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -1,68 +1,70 @@
 const teachers = [
-  'Nathan',
-  'Ed',
-  'Fabio',
-  'Phil',
-  'Carlo',
+  // 'Nathan',
+  // 'Ed',
+  // 'Fabio',
+  // 'Phil',
+  // 'Carlo',
   'Lewis',
-  'Luca'
+  // 'Luca'
 ];
  // NON MODIFICARE QUESTA VARIABILE
 
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
-const fourthTeacher = teachers[3];
+// const fourthTeacher = teachers[3];
 // console.log(fourthTeacher);
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
-teachers[4] = "Patrick";
+// teachers[4] = "Patrick";
 // console.log(teachers);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = teachers.pop();
+// const lastTeacher = teachers.pop();
 // console.log(lastTeacher);
 
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = teachers.shift();
+// const firstTeacher = teachers.shift();
 // console.log(firstTeacher);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
-teachers.push("Vanessa");
+// teachers.push("Vanessa");
 // console.log(teachers)
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
-teachers.unshift("Sarah");
-// console.log(teachers)
+// teachers.unshift("Sarah");
+console.log(teachers)
 
 
 // 7. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-let isFabioPresent = false;
-for(i = 0 ; i < 7 ; i++ ) {
-  if( teachers[i] ==="Fabio"){
-    isFabioPresent = true
-  }
-}
+// let isFabioPresent = false;
+// for(i = 0 ; i < 7 ; i++ ) {
+//   if( teachers[i] ==="Fabio"){
+//     isFabioPresent = true
+//   }
+// }
 // console.log(isFabioPresent);
 
 // 8. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
-const lewisIndex = teachers.indexOf("Lewis");
+// const lewisIndex = teachers.indexOf("Lewis");
 // console.log(lewisIndex);
 
 // 9. Unisci tutti gli insegnanti nell'array teachers in una stringa
 // separata da virgole e salvala nella variabile teachersString
 // const teachersString = null;
-const teachersString = teachers.toString()
-console.log(teachersString);
+// const teachersString = teachers.toString()
+// console.log(teachersString);
 
 // 10. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = true
+console.log(teachers.lenght());
 
-  if(teachers.lenght == 0){
-    isTeachersEmpty = false;
-  }
+const isTeachersEmpty = false
+
+  // if(teachers.length() == 0){
+  //   isTeachersEmpty = true;
+  // }
 
 console.log(isTeachersEmpty);
